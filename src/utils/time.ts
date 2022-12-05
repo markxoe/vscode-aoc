e7e689f6af6e50cdc78136b74501fcd074047b3c
Paul Gautreaux <paul@embdevspecialists.com>
2022-12-05 00:02:33 -0800

    Determine time as "Americas/New_York"

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,9 +1,11 @@
-import * as moment from "moment";
+import * as moment from "moment-timezone";
+
+const tz = "America/New_York";
 
 // Get current year
-export const getYear = () => moment().year();
+export const getYear = () => moment().tz(tz).year();
 
 // Get current day of the month
-export const getDay = () => moment().date();
+export const getDay = () => moment().tz(tz).date();
 
-export const isDecember = () => moment().month() === 12 - 1;
+export const isDecember = () => moment().tz(tz).month() === 12 - 1;
